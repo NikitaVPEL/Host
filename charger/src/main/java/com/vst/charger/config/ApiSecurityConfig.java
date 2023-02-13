ada1aea5934607cf9c1d542010e97563b2165fe9
nikit <nikit@192.168.0.122>
2023-02-13 10:10:13 +0530

    Nikita 13022023

--- charger/src/main/java/com/vst/charger/config/ApiSecurityConfig.java
+++ charger/src/main/java/com/vst/charger/config/ApiSecurityConfig.java
@@ -1,44 +1,43 @@
-package com.vst.charger.config;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@Configuration
-@EnableWebSecurity
-public class ApiSecurityConfig {
-	
-	String admin = "ADMIN";
-	String user = "USER";
-
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-
-		List<UserDetails> users = new ArrayList<>();
-		users.add(User.withUsername("user").password("{noop}user").roles(user).build());
-
-		users.add(User.withUsername("admin").password("{noop}admin").roles(admin).build());
-		
-		return new InMemoryUserDetailsManager(users);
-		
-	}
-
-
-	@SuppressWarnings("deprecation")
-	public void configure(HttpSecurity http) throws Exception {
-		http
-		.authorizeRequests()
-		
-		.requestMatchers("/charger").hasAnyRole(admin,user)
-		.requestMatchers("/charger/get/{chargerId}").hasRole(admin);
-	}
-	
-
-}
+//package com.vst.charger.config;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class ApiSecurityConfig {
+//	
+//	String admin = "ADMIN";
+//	String user = "USER";
+//
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsService() {
+//
+//		List<UserDetails> users = new ArrayList<>();
+//		users.add(User.withUsername("user").password("{noop}user").roles(user).build());
+//
+//		users.add(User.withUsername("admin").password("{noop}admin").roles(admin).build());
+//		
+//		return new InMemoryUserDetailsManager(users);
+//		
+//	}
+//
+//	@SuppressWarnings("deprecation")
+//	public void configure(HttpSecurity http) throws Exception {
+//		http
+//		.authorizeRequests()
+//		
+//		.requestMatchers("/charger").hasAnyRole(admin,user)
+//		.requestMatchers("/charger/get/{chargerId}").hasRole(admin);
+//	}
+//	
+//
+//}
