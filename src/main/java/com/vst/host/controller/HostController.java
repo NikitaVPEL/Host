f740cd7f737a7a9ce8563f53783a55e7bb6af056
Nikita <nikita.chakole@vpel.in>
2023-05-12 14:23:30 +0530

    added cross origin in controller

--- src/main/java/com/vst/host/controller/HostController.java
+++ src/main/java/com/vst/host/controller/HostController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import com.vst.host.service.HostServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequestMapping("manageHost")
 public class HostController {
